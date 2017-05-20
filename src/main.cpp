deec0fd11880fe5a1b3f189cff26da2228d6e4e3
David Lougheed <david.lougheed@gmail.com>
2017-05-20 11:04:01 -0400

    Rename freeRAM method.

--- src/main.cpp
+++ src/main.cpp
@@ -111,7 +111,7 @@ uint32_t uptime = 0;
 
 // Determine amount of free RAM.
 // - Retrieved 2017-05-19 (https://playground.arduino.cc/Code/AvailableMemory)
-int freeRam () {
+int freeRAM() {
     extern int __heap_start, *__brkval;
     int v;
     return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval);
